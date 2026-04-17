19eefde3e26633b2603e40e1ded6ac6a8665fb41
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-04-17 10:20:45 +0530

    April 17th Java Programs

---
+++ NotePad Programs and Assignments/Add.java
@@ -0,0 +1,13 @@
+class Addition2Num
+{
+public static void main(String args[])
+{
+	int a=200;
+	int b=100;
+	int c=a+b;
+ System.out.print("Sum of given Two Numbers:");
+ System.out.println(c);
+ 
+  }
+ }
+ 
---
+++ NotePad Programs and Assignments/Add_Memory.java
@@ -0,0 +1,10 @@
+class Addition2Num
+{
+public static void main(String args[])
+{
+	int a=200,b=100,c;
+	c=a+b;
+ System.out.print("Sum of given Two Numbers:" + c);
+  }
+ }
+ 
