9201801f49444a873f0c128d2413d7d85c32b488
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-04-18 10:30:49 +0530

    April 18th Java

---
+++ NotePad Programs and Assignments/Addition2NumFloat.java
@@ -0,0 +1,11 @@
+class Addition2NumFloat
+{
+public static void main(String args[])
+{
+float num1=22.33f;
+float num2=233.22f;
+float sum;
+sum=num1+num2;
+System.out.println("Addition:"+sum);
+}
+}
