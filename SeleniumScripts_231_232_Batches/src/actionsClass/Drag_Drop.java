6cd19e945a8a09f01b4de5763ca0981cc01fbb05
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-06-05 12:48:25 +0530

    Jun4th 5th Selenium Scripts

--- SeleniumScripts_231_232_Batches/src/actionsClass/Drag_Drop.java
+++ SeleniumScripts_231_232_Batches/src/actionsClass/Drag_Drop.java
@@ -6,14 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Drag_Drop {
-
-	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.navigate().to("https://demoqa.com/droppable");
-		Thread.sleep(3000);
+public class Drag_Drop extends BrowserConfig_Parent
+{
 		
+	public void DragDrop() throws InterruptedException
+	{
+		//BrowserConfig();
 		Actions actions = new Actions(driver);
 		
 		WebElement dragMe = driver.findElement(By.id("draggable"));
@@ -22,6 +20,17 @@ public class Drag_Drop {
 		actions.dragAndDrop(dragMe, dropHere).perform();
 		Thread.sleep(2000);
 		 driver.quit();
+	}
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Drag_Drop drag_Drop = new Drag_Drop();
+		drag_Drop.BrowserConfig();
+		drag_Drop.DragDrop();
+	
+		
+		
+		
 	}
 
 }
---
+++ SeleniumScripts_231_232_Batches/src/testNG_Scripts/TestNG_Annotations.java
@@ -0,0 +1,97 @@
+package testNG_Scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNG_Annotations {
+
+	WebDriver driver;
+
+	//@BeforeMethod   // 9am 4 Primary 12:30  High 9am 10am 11am 12am  
+	//@BeforeClass
+	@BeforeTest
+	public void BrowserOpen() throws InterruptedException {
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(2000);
+
+	}
+
+	@Test(priority=1)  //Test Case-1
+	public void SwagValidLogin() throws InterruptedException { // WOR and WOP [Insatnec or Non-Static]
+
+		WebElement username = driver.findElement(By.id("user-name"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement login = driver.findElement(By.id("login-button"));
+		
+		username.sendKeys("standard_user");
+		Thread.sleep(2000);
+		password.sendKeys("secret_sauce");
+		Thread.sleep(2000);
+		login.click();
+		Thread.sleep(3000);
+		WebElement threeLines=driver.findElement(By.id("react-burger-menu-btn"));
+		threeLines.click();
+		Thread.sleep(2000);
+		WebElement logout=driver.findElement(By.id("logout_sidebar_link"));
+		logout.click();
+		Thread.sleep(2000);
+		
+	}
+
+	@Test(priority=2) //Test Case-2
+	public void SwagInValidLogin() throws InterruptedException { // WOR and WOP [Insatnec or Non-Static]
+
+		WebElement username = driver.findElement(By.id("user-name"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement login = driver.findElement(By.id("login-button"));
+
+		username.sendKeys("gdfgdfg");
+		Thread.sleep(2000);
+		password.sendKeys("sdgdsgs");
+		Thread.sleep(2000);
+		login.click();
+		Thread.sleep(3000);
+
+	}
+	
+	@Test(priority=3) //Test Case-2
+	public void SwagBlankLogin() throws InterruptedException { // WOR and WOP [Insatnec or Non-Static]
+
+		WebElement username = driver.findElement(By.id("user-name"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement login = driver.findElement(By.id("login-button"));
+
+		username.clear();
+		Thread.sleep(2000);
+		username.sendKeys("");
+		Thread.sleep(2000);
+		password.clear();
+		Thread.sleep(2000);
+		password.sendKeys("");
+		Thread.sleep(2000);
+		login.click();
+		Thread.sleep(3000);
+
+	}
+
+	//@AfterMethod
+	//@AfterClass
+	@AfterTest
+	public void BrowserClose() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+}
